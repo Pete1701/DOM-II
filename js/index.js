d5fa3fd7ee50b09fb7e89f450665b6e33b1ae7d5
Pete1701 <pete.petrongonas@pirron.one>
2020-02-11 22:58:27 +0100

    4 events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+
+const destination = document.querySelector('.content-destination img');
+destination.addEventListener('mouseover', () =>{
+    destination.style.transform = 'scale(1.5)';
+    destination.style.transition = '0.5s';
+})
+destination.addEventListener('wheel', () =>{
+    destination.style.transform = 'scale(1)';    
+})
+
+const funBus = document.querySelector('.intro img');
+funBus.addEventListener('mousemove', () =>{
+    funBus.style.transform = 'rotate(0deg)';
+})
+funBus.addEventListener('dblclick', () =>{
+    funBus.style.transform = 'rotate(-20deg)';
+})
