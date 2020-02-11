148ab7920ea788d59811e3a5e6858fc492dd2e54
Pete1701 <pete.petrongonas@pirron.one>
2020-02-11 23:41:45 +0100

    8 events

--- js/index.js
+++ js/index.js
@@ -10,9 +10,25 @@ destination.addEventListener('wheel', () =>{
 })
 
 const funBus = document.querySelector('.intro img');
-funBus.addEventListener('mousemove', () =>{
+funBus.addEventListener('mouseout', () =>{
     funBus.style.transform = 'rotate(0deg)';
 })
 funBus.addEventListener('dblclick', () =>{
     funBus.style.transform = 'rotate(-20deg)';
+})
+
+const background = document.querySelector('html');
+background.addEventListener('keydown', () =>{
+    background.style.backgroundColor = 'lightskyblue';
+})
+background.addEventListener('keyup', () =>{
+    background.style.backgroundColor = 'white';
+})
+
+const titles = document.querySelector('h2');
+titles.addEventListener('copy', () =>{
+    titles.style.fontWeight = '1000';
+})
+titles.addEventListener('mousemove', () =>{
+    titles.style.fontWeight = '500';
 })
